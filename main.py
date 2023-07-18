b4a16ef000d2963066290e6b902e91d4e41bbb93
PatrikPatrik2 <42711908+PatrikPatrik2@users.noreply.github.com>
2023-07-18 09:40:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def read_root():
 @app.post("/")
 def analyse_email(email: Email):
     content = email.content
-    query = f"From the mail below, exract what shall be quoted using the Price List, select the items that yields the lowest total cost, only the quantities from the price list should be used and combined to fulfill the requested quotation (10 100ft = 10 times 1000ft + 100ft). You are working at the sales office at Uponor and your name in Pamela Anderson, write a friendly quotation to the person signing the mail. If there are items in the request that are not included in the price list note that the price for those items will be given later. Include the price for the items that could be found in the price list. If the request is using meters, convert it to ft before calculating the cost: {content} "
+    query = f"From the mail below, extract what shall be quoted using the Price List, select the items that yields the lowest total cost, only the quantities from the price list should be used and combined to fulfill the requested items You are working at the sales office at Uponor and your name in Pamela Anderson, write a friendly quotation to the person signing the mail. If there are items in the request that are not included in the price list note that the price for those items will be given later. Include the price for the items that could be found in the price list. If the request is using meters, convert it to ft before calculating the cost: {content} "
     #system_message="price list: \n AquaPEX Pex pipe 1/2\": 1 usd/ft \n  AquaPEX Pex pipe 3/4\" : 1.3 usd/ft \n AquaPEX Pex pipe 3\" : 2 usd/ft \n Fitting 1/2\" : 2 usd/ft \n Fitting 3/4\" : 3 usd/ft \n Fitting 1\" : 4 usd/ft \n"
     system_message="""
 Price List:
